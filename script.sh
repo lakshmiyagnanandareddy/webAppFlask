a17d7082a12356d1f7fcd21a4ec342bf43a1475d
lakshmiyagnanandareddy <99638103+lakshmiyagnanandareddy@users.noreply.github.com>
2023-08-31 19:37:46 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,1 @@
+docker run -it -p 9099:9099 nandu9948/jenkins_maven && yum intall git -y && git clone https://github.com/lakshmiyagnanandareddy/webAppFlask.git && cd webAppFlask && git checkout dev && mvn jetty:run & sleep 20s && mvn package
