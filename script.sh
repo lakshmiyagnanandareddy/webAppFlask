004b9c54e8e2f96035f5f4f1e3e3410e86fa1a9e
lakshmiyagnanandareddy <99638103+lakshmiyagnanandareddy@users.noreply.github.com>
2023-08-31 20:47:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-unstash 'gitRepo' && docker run -it -p 9099:9099 nandu9948/jenkins_maven && yum intall git -y && git clone https://github.com/lakshmiyagnanandareddy/webAppFlask.git && cd webAppFlask && git checkout dev && mvn jetty:run & sleep 20s && mvn package
+ docker run -it -p 9099:9099 nandu9948/jenkins_maven && yum intall git -y && git clone https://github.com/lakshmiyagnanandareddy/webAppFlask.git && cd webAppFlask && git checkout dev && mvn jetty:run & sleep 20s && mvn package
