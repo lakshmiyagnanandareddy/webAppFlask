64942c644a8496bcad134a595e50507d60dab028
lakshmiyagnanandareddy <99638103+lakshmiyagnanandareddy@users.noreply.github.com>
2023-09-03 19:10:42 +0530

    Update WebAppTest.java

--- src/test/java/WebAppTest.java
+++ src/test/java/WebAppTest.java
@@ -1,7 +1,9 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.Assert.*;
@@ -28,14 +30,42 @@ public class WebAppTest {
     }
 
     @Test
-    public void testWebPageContent() {
+    public void testFruitsAndVegetablesCard() {
         driver.get("http://54.87.244.239:9099/"); // Replace with your server URL
-        String pageSource = driver.getPageSource();
+        // String pageSource = driver.getPageSource();
 
-        assertTrue(pageSource.contains("Lakshmi reddy's webserver"));
-        assertTrue(pageSource.contains("`ifconfig`"));
+        // Locate the elements within the "Fruits & Vegetables" card
+        WebElement cardTitle = driver.findElement(By.xpath("//div[@class='card-title fw-bold']"));
+        WebElement discountText = driver.findElement(By.xpath("//p[text()='Get Upto 30% off']"));
+        WebElement shopNowButton = driver.findElement(By.xpath("//a[@class='btn-danger p-2 text-decoration-none rounded']"));
 
+        // Assert that the expected content is present in the card
+        assertTrue(cardTitle.isDisplayed());
+        assertEquals("Fruits & Vegetables", cardTitle.getText());
+        assertTrue(discountText.isDisplayed());
+        assertEquals("Get Upto 30% off", discountText.getText());
+        assertTrue(shopNowButton.isDisplayed());
+        assertEquals("Shop Now", shopNowButton.getText());
         // You can add more assertions to test other parts of the HTML content
+    }
+    
+    @Test
+    public void testAllGroceriesCard() {
+        driver.get("http://54.87.244.239:9099/"); // Replace with your server URL
+        // String pageSource = driver.getPageSource();
+
+        // Locate the elements within the "All Groceries" card
+        WebElement cardTitle = driver.findElement(By.xpath("//div[@class='card-title fw-bold']"));
+        WebElement discountText = driver.findElement(By.xpath("//p[text()='Get Flat Upto Rs.300off']"));
+        WebElement orderNowButton = driver.findElement(By.xpath("//a[@class='btn-danger p-2 text-decoration-none rounded']"));
+
+        // Assert that the expected content is present in the card
+        assertTrue(cardTitle.isDisplayed());
+        assertEquals("Order Value of 1200rs", cardTitle.getText());
+        assertTrue(discountText.isDisplayed());
+        assertEquals("Get Flat Upto Rs.300off", discountText.getText());
+        assertTrue(orderNowButton.isDisplayed());
+        assertEquals("Order Now", orderNowButton.getText());
     }
 }
 
