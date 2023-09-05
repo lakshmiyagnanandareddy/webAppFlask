77ed53eb52d69ea202a22ea46d23b92ed9e33536
lakshmiyagnanandareddy <99638103+lakshmiyagnanandareddy@users.noreply.github.com>
2023-09-06 00:40:36 +0530

    E-commerce Project Prod

--- src/test/java/WebAppTest.java
+++ src/test/java/WebAppTest.java
@@ -31,7 +31,7 @@ public class WebAppTest {
 
     @Test
     public void testFruitsAndVegetablesCard() {
-        driver.get("http://3.91.99.123:9099/"); // Replace with your server URL
+        driver.get("http://54.145.149.125:9099/"); // Replace with your server URL
         // String pageSource = driver.getPageSource();
         
          // Locate the card title and content
@@ -40,7 +40,7 @@ public class WebAppTest {
     }
     @Test
     public void testAllGroceriesCard() {
-        driver.get("http://54.91.204.246:9099/"); // Replace with your server URL
+        driver.get("http://54.145.149.125:9099/"); // Replace with your server URL
         // String pageSource = driver.getPageSource();
 
         // Locate the elements within the "All Groceries" card
